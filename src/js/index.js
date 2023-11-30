b7ad1cf6916ece223f8685192aa08578a1cdf919
Bruno Lima <brunoadl88@gmail.com>
2023-11-30 22:26:02 +0000

    routes completed

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,9 @@ const router = new Router();
 router.add("/", "/src/pages/home.html");
 router.add("/universe", "/src/pages/universe.html");
 router.add("/explorer", "/src/pages/explorer.html");
+router.add(404, "/src/pages/404.html");
+
+router.handle();
+
+window.onpopstate = router.handle();
+window.route = () => router.route();
